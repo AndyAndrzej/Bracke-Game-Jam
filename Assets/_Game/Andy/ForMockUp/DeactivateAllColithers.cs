3e5c10a951365f7b379d78542e767223d61fdc51
Andruplay <gdsg>
2021-02-21 00:26:02 +0100

    adding new slime and eating mech

---
+++ Assets/_Game/Andy/ForMockUp/DeactivateAllColithers.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateAllColithers : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private BoxCollider[] _collithers;
+    void Start()
+    {
+        _collithers = this.GetComponentsInChildren<BoxCollider>();
+        for (int i = 0; i < _collithers.Length; i++)
+        {
+            _collithers[i].enabled = false;
+            Destroy(_collithers[i], 2);
+        }
+    }
+
+
+}
