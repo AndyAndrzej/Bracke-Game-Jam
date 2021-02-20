a34c1d307f5ddebdadfef1988b81049b62843997
Bartosz Galiński <bgalinski@edu.cdv.pl>
2021-02-20 21:03:24 +0100

    Added FMOD and FMOD related sounds and events

---
+++ Assets/_Game/Bartosz/FMODPlayerEvents.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD.Studio;
+
+public class FMODPlayerEvents : MonoBehaviour
+{
+    [SerializeField] private string _jump;
+    [SerializeField] private string _connect;
+    FMOD.Studio.EventInstance JumpEvent;
+    FMOD.Studio.EventInstance ConnectEvent;
+
+
+    private void Start()
+    {
+        ConnectEvent = FMODUnity.RuntimeManager.CreateInstance(_connect);
+        JumpEvent = FMODUnity.RuntimeManager.CreateInstance(_jump);
+    }
+    private void Update()
+    {
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(ConnectEvent, GetComponent<Transform>(), GetComponent<Rigidbody>());
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(JumpEvent, GetComponent<Transform>(), GetComponent<Rigidbody>());
+    }
+
+    void PlayJumpEvent()
+    {
+        JumpEvent.start();
+    }
+
+    void PlayConnectEvent()
+    {
+        ConnectEvent.start();
+    }
+}
+
---
+++ Assets/_Game/Bartosz/FMODSoundtrack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class FMODSoundtrack : MonoBehaviour
+{
+    [SerializeField] private string _selectSoundtrack;
+    FMOD.Studio.EventInstance soundtrack;
+
+    private void Awake()
+    {
+        soundtrack = FMODUnity.RuntimeManager.CreateInstance(_selectSoundtrack);
+        soundtrack.start();
+    }
+
+    public void OnDestroy()
+    {
+        soundtrack.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        soundtrack.release();
+    }
+}
